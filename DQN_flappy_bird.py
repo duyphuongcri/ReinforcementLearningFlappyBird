7d2bf4377d5391ed8e9816f6ff7a1e2a56f8b863
duyphuongdao <duyphuongcri@gmail.com>
2020-05-05 17:31:30 +0900

    no message

---
+++ DQN_flappy_bird.py
@@ -0,0 +1,237 @@
+import tensorflow as tf 
+import numpy as np 
+import random
+from collections import deque 
+import sys
+sys.path.append("game/")
+import wrapped_flappy_bird as game
+import cv2
+
+# Hyper Parameters:
+FRAME_PER_ACTION = 1
+ACTIONS = 2 # number of valid actions
+GAMMA = 0.95 # decay rate of past observations
+OBSERVE = 50000 # timesteps to observe before training
+EXPLORE = 1000000. # frames over which to anneal epsilon
+FINAL_EPSILON = 0.001#0.001 # final value of epsilon
+INITIAL_EPSILON = 0.001#0.01 # starting value of epsilon
+REPLAY_MEMORY = 1000000 # number of previous transitions to remember
+BATCH_SIZE = 32 # size of minibatch
+UPDATE_TIME = 10000
+
+class DQN:
+
+    def __init__(self,actions):
+        # init replay memory
+        self.replayMemory = deque()
+        # init some parameters
+        self.timeStep = 0
+        self.epsilon = INITIAL_EPSILON
+        self.actions = actions
+        # init Q network
+        self.stateInput,self.QValue,self.W_conv1,self.b_conv1,self.W_conv2,self.b_conv2,self.W_conv3,self.b_conv3,self.W_fc1,self.b_fc1,self.W_fc2,self.b_fc2 = self.createQNetwork()
+
+        # init Target Q Network
+        self.stateInputT,self.QValueT,self.W_conv1T,self.b_conv1T,self.W_conv2T,self.b_conv2T,self.W_conv3T,self.b_conv3T,self.W_fc1T,self.b_fc1T,self.W_fc2T,self.b_fc2T = self.createQNetwork()
+
+        self.copyTargetQNetworkOperation = [self.W_conv1T.assign(self.W_conv1),self.b_conv1T.assign(self.b_conv1),self.W_conv2T.assign(self.W_conv2),self.b_conv2T.assign(self.b_conv2),self.W_conv3T.assign(self.W_conv3),self.b_conv3T.assign(self.b_conv3),self.W_fc1T.assign(self.W_fc1),self.b_fc1T.assign(self.b_fc1),self.W_fc2T.assign(self.W_fc2),self.b_fc2T.assign(self.b_fc2)]
+
+        self.createTrainingMethod()
+
+        # saving and loading networks
+        self.saver = tf.train.Saver()
+        self.session = tf.InteractiveSession()
+        self.session.run(tf.initialize_all_variables())
+        checkpoint = tf.train.get_checkpoint_state("saved_networks")
+        if checkpoint and checkpoint.model_checkpoint_path:
+            self.saver.restore(self.session, checkpoint.model_checkpoint_path)
+            print("Successfully loaded:", checkpoint.model_checkpoint_path)
+        else:
+            print("Could not find old network weights")
+
+
+    def createQNetwork(self):
+        # network weights
+        W_conv1 = self.weight_variable([8,8,4,32])
+        b_conv1 = self.bias_variable([32])
+
+        W_conv2 = self.weight_variable([4,4,32,64])
+        b_conv2 = self.bias_variable([64])
+
+        W_conv3 = self.weight_variable([3,3,64,64])
+        b_conv3 = self.bias_variable([64])
+
+        W_fc1 = self.weight_variable([1600,512])
+        b_fc1 = self.bias_variable([512])
+
+        W_fc2 = self.weight_variable([512, self.actions])
+        b_fc2 = self.bias_variable([self.actions])
+
+        # input layer
+
+        stateInput = tf.placeholder("float",[None, 80, 80, 4])
+
+        # hidden layers
+        h_conv1 = tf.nn.relu(self.conv2d(stateInput,W_conv1,4) + b_conv1)
+        h_pool1 = self.max_pool_2x2(h_conv1)
+
+        h_conv2 = tf.nn.relu(self.conv2d(h_pool1, W_conv2, 2) + b_conv2)
+
+        h_conv3 = tf.nn.relu(self.conv2d(h_conv2,W_conv3,1) + b_conv3)
+
+        h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
+
+        h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
+        # Q Value layer
+        QValue = tf.matmul(h_fc1,W_fc2) + b_fc2
+
+        return stateInput,QValue,W_conv1,b_conv1,W_conv2,b_conv2,W_conv3,b_conv3,W_fc1,b_fc1,W_fc2,b_fc2
+
+    def copyTargetQNetwork(self):
+        self.session.run(self.copyTargetQNetworkOperation)
+
+    def createTrainingMethod(self):
+        self.actionInput = tf.placeholder("float",[None,self.actions])
+        self.yInput = tf.placeholder("float", [None]) 
+        Q_Action = tf.reduce_sum(tf.multiply(self.QValue, self.actionInput), reduction_indices = 1)
+        self.cost = tf.reduce_mean(tf.square(self.yInput - Q_Action))
+        self.trainStep = tf.train.RMSPropOptimizer(0.00025,0.99,0.0,1e-6).minimize(self.cost)
+
+
+    def trainQNetwork(self):
+
+        # Step 1: obtain random minibatch from replay memory
+        minibatch = random.sample(self.replayMemory,BATCH_SIZE)
+        state_batch = [data[0] for data in minibatch]
+        action_batch = [data[1] for data in minibatch]
+        reward_batch = [data[2] for data in minibatch]
+        nextState_batch = [data[3] for data in minibatch]
+
+        # Step 2: calculate y 
+        y_batch = []
+        QValue_batch = self.QValueT.eval(feed_dict={self.stateInputT:nextState_batch})
+        for i in range(0,BATCH_SIZE):
+            terminal = minibatch[i][4]
+            if terminal:
+                y_batch.append(reward_batch[i])
+            else:
+                y_batch.append(reward_batch[i] + GAMMA * np.max(QValue_batch[i]))
+
+        self.trainStep.run(feed_dict={
+            self.yInput : y_batch,
+            self.actionInput : action_batch,
+            self.stateInput : state_batch
+            })
+
+		# save network every 100000 iteration
+        if self.timeStep % 10000 == 0:
+            self.saver.save(self.session, 'saved_networks/' + 'network' + '-dqn', global_step = self.timeStep)
+
+        if self.timeStep % UPDATE_TIME == 0:
+            self.copyTargetQNetwork()
+
+    def interfere(self, nextObservation):
+        self.currentState = np.append(nextObservation,self.currentState[:,:,:3],axis = 2)
+		
+    def setPerception(self,nextObservation,action,reward,terminal):
+        newState = np.append(nextObservation,self.currentState[:,:,:3],axis = 2)
+        self.replayMemory.append((self.currentState,action,reward,newState,terminal))
+        if len(self.replayMemory) > REPLAY_MEMORY:
+            self.replayMemory.popleft()
+        if self.timeStep > OBSERVE:
+            # Train the network
+            self.trainQNetwork()
+
+        # print info
+        state = ""
+        if self.timeStep <= OBSERVE:
+            state = "observe"
+        elif self.timeStep > OBSERVE and self.timeStep <= OBSERVE + EXPLORE:
+            state = "explore"
+        else:
+            state = "train"
+
+        print("TIMESTEP", self.timeStep, "/ STATE", state, "/ EPSILON", self.epsilon)
+
+        self.currentState = newState
+        self.timeStep += 1
+
+    def getAction(self):
+        QValue = self.QValue.eval(feed_dict= {self.stateInput:[self.currentState]})[0]
+        action = np.zeros(self.actions)
+        action_index = 0
+        if self.timeStep % FRAME_PER_ACTION == 0:
+            if random.random() <= self.epsilon:
+                action_index = random.randrange(self.actions)
+                action[action_index] = 1
+            else:
+                action_index = np.argmax(QValue)
+                action[action_index] = 1
+        else:
+            action[1] = 1 # do nothing
+
+        # change episilon
+        if self.epsilon > FINAL_EPSILON and self.timeStep > OBSERVE:
+            self.epsilon -= (INITIAL_EPSILON - FINAL_EPSILON)/EXPLORE
+
+        return action
+
+    def setInitState(self,observation):
+        self.currentState = np.stack((observation, observation, observation, observation), axis = 2)
+
+    def weight_variable(self,shape):
+        return tf.Variable(tf.truncated_normal(shape, stddev = 0.01))
+
+    def bias_variable(self,shape):
+        return tf.Variable(tf.constant(0.01, shape = shape))
+
+    def conv2d(self,x, W, stride):
+        return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
+
+    def max_pool_2x2(self,x):
+        return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+
+# preprocess raw image to 80*80 gray image
+def preprocess(observation):
+	observation = cv2.cvtColor(cv2.resize(observation, (80, 80)), cv2.COLOR_BGR2GRAY)
+	ret, observation = cv2.threshold(observation,1,255,cv2.THRESH_BINARY)
+	return np.reshape(observation,(80,80,1))
+
+def playFlappyBird(mode):
+
+    env = game.GameState()
+    Deep_Q_Network = DQN(ACTIONS)
+
+    do_nothing = np.zeros(ACTIONS)
+    do_nothing[1] = 1
+    observation0, reward0, terminal = env.frame_step(do_nothing)
+    observation0 = cv2.cvtColor(cv2.resize(observation0, (80, 80)), cv2.COLOR_BGR2GRAY)
+    ret, observation0 = cv2.threshold(observation0,1,255,cv2.THRESH_BINARY)
+    Deep_Q_Network.setInitState(observation0)
+
+    while 1!= 0:
+        action = Deep_Q_Network.getAction()
+        nextObservation,reward,terminal = env.frame_step(action)
+        nextObservation = preprocess(nextObservation)
+        # Train
+        if mode == "train":
+            INITIAL_EPSILON = 1.0
+            Deep_Q_Network.setPerception(nextObservation,action,reward,terminal)
+        # Interfere
+        if mode == "test":
+            Deep_Q_Network.interfere(nextObservation)
+        cv2.imshow("", nextObservation )
+        if cv2.waitKey(1) == 27:
+            break
+
+import argparse
+parser = argparse.ArgumentParser()
+
+if __name__ == '__main__':
+    parser.add_argument('--mode', required=True,
+                        metavar="",
+                        help='train or test')
+    args = parser.parse_args()
+    playFlappyBird(args.mode)
+
+cv2.destroyAllWindows()
